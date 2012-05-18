63bd2c5968b5336b070380a521ec581b9b2a9cc0
PriyankaG <priyanka.ges@gmail.com>
2012-05-18 21:41:19 +0530

    test commit

---
+++ EmotionalJourney/simpletable.h
@@ -0,0 +1,13 @@
+//
+//  simpletable.h
+//  EmotionalJourney
+//
+//  Created by Administrator on 18/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface simpletable : UITableViewController
+
+@end
